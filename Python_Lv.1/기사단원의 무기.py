c8c326dbe996011e91efa2a452a48385ad0a3978
ファン <syhwang.web@gmail.com>
2022-11-22 20:51:33 +0900

    sync

--- Python_Lv.1/기사단원의 무기.py
+++ Python_Lv.1/기사단원의 무기.py
@@ -1,14 +1,11 @@
-# include <stdio.h>
-# include <stdbool.h>
-# include <stdlib.h>
 def get_divider_cnt(number):
     d = []
 
-    for i in range(1, int(number ** 0.5 + 1)):
+    for i in range(1, int(number**0.5 + 1)):
         if number % i == 0:
             d.append(i)
-            if i != number//i:
-                d.append(number//i)
+            if i != number // i:
+                d.append(number // i)
     return len(d)
 
 
@@ -16,7 +13,7 @@ def solution(number, limit, power):
     answer = 0
     iron_weight = 0
 
-    for i in range(1, number+1):
+    for i in range(1, number + 1):
         curr_divider = get_divider_cnt(i)
         if curr_divider > limit:
             iron_weight = power
