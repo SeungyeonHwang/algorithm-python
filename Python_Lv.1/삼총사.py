13bf860da34e7991d720cbf4ace5bf5cf0728fde
Hwang <hwang@infoscience.co.jp>
2022-12-02 16:46:40 +0900

    삼총사

--- Python_Lv.1/삼총사.py
+++ Python_Lv.1/삼총사.py
@@ -1,11 +1,8 @@
-"""
-개인별 정수 번호 
-배열 pick -> ? + ? + ? = 0 => 삼총사
-삼총사를 만들 수 있는 경우의 수
-"""
+from itertools import combinations
 
 
 def solution(number):
-    answer = 0
+    return list(map(sum, list(combinations(number, 3)))).count(0)
 
-    return answer
+
+print(solution([-2, 3, 0, 2, -5]))
